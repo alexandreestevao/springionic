84021a550a9976844e3e3e357f5eef403efd8a62
Alexandre Estevao <alexandresestevao@gmail.com>
2022-02-17 17:01:58 -0300

    Project created

---
+++ backend/src/test/java/com/alexandreestevao/springionic/SpringionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreestevao.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringionicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
